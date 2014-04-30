9c67105865e26de9cb2e9370b059f92ba1c96d30
rhutterer <roman.hutterer@cyberhouse.at>
2014-04-30 11:09:20 +0200

    initial commit for github

---
+++ app/code/community/Multibyte_VATFix/Helper/Data.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @category Multibyte
+ * @package Multibyte_VatFix
+ * @author Roman Hutterer <info@multibyte.at>
+ */
+class Multibyte_VATFix_Helper_Data extends Mage_Customer_Helper_Data {
+
+    const VAT_VALIDATION_WSDL_URL = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+
+    /**
+     * override the checkVatNumber from CustomerHelper
+     * @param string $countryCode
+     * @param string $vatNumber
+     * @param string $requesterCountryCode
+     * @param string $requesterVatNumber
+     * @return boolean
+     */
+    public function checkVatNumber($countryCode, $vatNumber, $requesterCountryCode = '', $requesterVatNumber = '') {
+        //@RHU take the VAT without the first two signs (which are the countrycode)
+        if ($this->includesCountryCode($vatNumber)) {
+            $vatNumber = substr(str_replace(' ', '', trim($vatNumber)), 2);
+        }
+        return parent::checkVatNumber($countryCode, $vatNumber, $requesterCountryCode, $requesterVatNumber);
+    }
+
+    /**
+     * Create SOAP client based on VAT validation service WSDL
+     *
+     * @param boolean $trace
+     * @return SoapClient
+     */
+    protected function _createVatNumberValidationSoapClient($trace = false) {
+        return new SoapClient(self::VAT_VALIDATION_WSDL_URL, array('trace' => $trace));
+    }
+
+    /**
+     * parse the VAT if it includes the countrycode
+     * @param string $vatNumber
+     * @return boolean
+     */
+    public function includesCountryCode($vatNumber) {
+        $countryCode = substr(str_replace(' ', '', trim($vatNumber)), 0, 2);
+        if (array_key_exists(strtoupper($countryCode), Mage::helper('multibyte_vatfix/countries')->getCountries())) {
+            return true;
+        }
+        return;
+    }
+
+}
