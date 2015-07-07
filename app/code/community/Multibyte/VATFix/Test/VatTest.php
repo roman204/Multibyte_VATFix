8f2e34165f60d0740cdef2ea30be295644a30794
Roman Hutterer <roman.hutterre@cyberhouse.at>
2015-07-07 09:12:07 +0200

    add test

---
+++ app/code/community/Multibyte/VATFix/Test/VatTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rhutterer
+ * Date: 07.07.15
+ * Time: 09:10
+ */
+class Multibyte_VATFix_Test_VatTest extends EcomDev_PHPUnit_Test_Case_Config {
+
+
+    /**
+     * @test
+     */
+
+    public function codePool() {
+        $this->assertModuleCodePool('community');
+    }
+}
